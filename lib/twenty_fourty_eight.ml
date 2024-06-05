d6924b5758769b43bbf58c2b35d7c958d7eebe87
midorishibukawa <midori@shibukawa.io>
2024-06-05 12:23:47 -0300

    implement move function

--- lib/twenty_fourty_eight.ml
+++ lib/twenty_fourty_eight.ml
@@ -24,6 +24,7 @@ end
 
 module Game(Params : GameParams) : Game = struct
     type direction = Up | Down | Left | Right
+    type axis = Horizontal | Vertical
     type cell = { value : int ; position : int } 
     type t = cell list
 
@@ -39,8 +40,23 @@ module Game(Params : GameParams) : Game = struct
         value and position lists empty *)
     let empty_game = []
 
+    let get_pos { position ; _ } = position
+    
+    let rec xy_of_idx ?(y=0) x =
+        if x < Params.size
+        then x, y
+        else xy_of_idx ~y:(y + 1) (x - Params.size)
+    
     (** set of all possible positions a cell can have *)
     let all_pos_set = 0 --^ cell_qty |> IntSet.of_enum
+
+    (** map where all possible positions a cell can have are the keys,
+        while their cartesian coordinates are the values *)
+    let all_pos_map =
+        let i_to_xy acc i = IntMap.add i (xy_of_idx i) acc in
+        all_pos_set
+        |> IntSet.elements
+        |> List.fold i_to_xy IntMap.empty
 
     (** generates a new cell with a value
         between 0 and 1 on any random empty space *)
@@ -61,6 +77,57 @@ module Game(Params : GameParams) : Game = struct
     (** creates a new game with a single random cell *)
     let new_game () = generate_cell empty_game
 
-    (** TODO *)
-    let move _dir game = game
+    (** moves all cells into the given direction,
+        generates a new cell,
+        and returns the new game state
+     *)
+    let move dir game =
+        let axis =
+            match dir with
+            | Left | Right -> Horizontal
+            | Up   | Down  -> Vertical in
+        let line_arr = Array.init Params.size (fun _ -> []) in
+        let update_arr arr { value ; position } = 
+            let select_i (x, y) = if axis = Vertical then x else y in
+            let i = IntMap.find position all_pos_map |> select_i in
+            arr.(i) <- value::arr.(i);
+            arr in
+        let get_j' j =
+            match dir with
+            | Down | Right -> Params.size - j - 1
+            | Up   | Left  -> j in
+        let is_rev = dir = Down || dir = Right in
+        let rec arr_to_cells ?(acc=[]) ?(j=0) i values =
+            let j' = get_j' j in
+            let x, y = if axis = Vertical then i, j' else j', i in
+            let position = y * Params.size + x in
+            match values with
+            | [] -> (if is_rev then List.rev else Fun.id) acc 
+            | value_a::value_b::values' ->
+                    let accd, values'' =
+                        if value_a = value_b
+                        then 1, values'
+                        else 0, value_b::values' in
+                    let acc = { value = value_a + accd ; position }::acc in
+                    let j = j + 1 in
+                    arr_to_cells ~acc ~j i values''
+            | value::values'' -> 
+                    let acc = { value ; position }::acc in
+                    let j = j + 1 in
+                    arr_to_cells ~acc ~j i values''
+            in
+        let set_of_game = IntSet.of_list % List.map get_pos  in
+        let game_set = set_of_game game in
+        let generate_cell_if_moved game' =
+            if IntSet.equal game_set (set_of_game game')
+            then game'
+            else generate_cell game' in
+        game
+        |> List.fold update_arr line_arr
+        |> Array.map (if is_rev then Fun.id else List.rev)
+        |> Array.mapi arr_to_cells
+        |> Array.to_list
+        |> List.flatten
+        |> generate_cell_if_moved
+        |> List.sort (fun a b -> a.position - b.position)
 end
--- test/test_twenty_fourty_eight.ml
+++ test/test_twenty_fourty_eight.ml
@@ -10,6 +10,31 @@ end
 
 module Game = Game(GameParams)
 
+let top_row = Fun.id
+let bottom_row i = size * (size - 1) + i
+let right_col i = size * (i + 1) - 1
+let left_col = ( * ) size 
+
+let map2cell value position = Game.({ value ; position })
+
+let test_move dir ?(cell_qty=size) starting_f expected_f =
+    let positions = List.init GameParams.size starting_f in
+    let expected = List.init GameParams.size expected_f |> IntSet.of_list in
+    let values = List.make GameParams.size 0 in
+    let game = List.map2 map2cell values positions in
+    let game' = Game.move dir game in
+    let assert_length = List.length game' = cell_qty in
+    let check_val Game.({ value ; _ }) = value = 0 || value = 1 in 
+    let assert_values = List.for_all check_val game' in
+    let assert_positions =
+        let get_pos Game.({ position ; _ }) = position in
+        let actual = game' |> List.map get_pos |> IntSet.of_list in
+        IntSet.subset expected actual in
+    assert_length
+    && assert_values
+    && assert_positions
+
+
 let%test "new_game" = 
     let game = Game.new_game () in
     let assert_cell_list_length = 
@@ -22,3 +47,29 @@ let%test "new_game" =
     assert_cell_list_length
     && assert_cell_value
     && assert_cell_position
+
+let%test "move up" = test_move Game.Up ~cell_qty:(size + 1) bottom_row top_row
+let%test "move up no move" = test_move Game.Up top_row top_row
+let%test "move down" = test_move Game.Down ~cell_qty:(size + 1) top_row bottom_row
+let%test "move down no move" = test_move Game.Down bottom_row bottom_row
+let%test "move left" = test_move Game.Left ~cell_qty:(size + 1) right_col left_col
+let%test "move left no move" = test_move Game.Left left_col left_col
+let%test "move right" = test_move Game.Right ~cell_qty:(size + 1) left_col right_col
+let%test "move right no move" = test_move Game.Right right_col right_col
+
+let%test "merge simple" =
+    let values = [0 ; 0] in
+    let positions = [0 ; 1] in
+    let game = List.map2 map2cell values positions in
+    let game' = Game.move Game.Left game in
+    let assert_length = List.length game' = 2 in
+    assert_length
+
+let%test "merge middle" =
+    let values = [0 ; 1 ; 1 ; 0] in
+    let positions = [0 ; 1 ; 2 ; 3] in
+    let game = List.map2 map2cell values positions in
+    let game' = Game.move Game.Right game in
+    let assert_length = List.length game' = 4 in 
+    assert_length
+
